ef090f54ecbeaea26f1456cba9ee0839143afda2
տիգրան <tigran.contact@gmail.com>
2023-07-10 13:28:31 +0400

    Create languages.js

---
+++ languages.js
@@ -0,0 +1,922 @@
+export const LANGUAGES = [
+  {
+    code: 'aa',
+    name: 'Afar',
+    icon: '🇪🇹',
+  },
+  {
+    code: 'ab',
+    name: 'Abkhazian',
+    icon: '🇬🇪',
+  },
+  {
+    code: 'ae',
+    name: 'Avestan',
+    icon: '🇮🇷',
+  },
+  {
+    code: 'af',
+    name: 'Afrikaans',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'ak',
+    name: 'Akan',
+    icon: '🇬🇭',
+  },
+  {
+    code: 'am',
+    name: 'Amharic',
+    icon: '🇪🇹',
+  },
+  {
+    code: 'an',
+    name: 'Aragonese',
+    icon: '',
+  },
+  {
+    code: 'ar',
+    name: 'Arabic',
+    icon: '🇪🇬',
+  },
+  {
+    code: 'as',
+    name: 'Assamese',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'av',
+    name: 'Avaric',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'ay',
+    name: 'Aymara',
+    icon: '🇧🇴',
+  },
+  {
+    code: 'az',
+    name: 'Azerbaijani',
+    icon: '🇦🇿',
+  },
+  {
+    code: 'ba',
+    name: 'Bashkir',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'be',
+    name: 'Belarusian',
+    icon: '🇧🇾',
+  },
+  {
+    code: 'bg',
+    name: 'Bulgarian',
+    icon: '🇧🇬',
+  },
+  {
+    code: 'bh',
+    name: 'Bihari languages',
+    icon: '',
+  },
+  {
+    code: 'bi',
+    name: 'Bislama',
+    icon: '🇻🇺',
+  },
+  {
+    code: 'bm',
+    name: 'Bambara',
+    icon: '🇲🇱',
+  },
+  {
+    code: 'bn',
+    name: 'Bengali',
+    icon: '🇧🇩',
+  },
+  {
+    code: 'bo',
+    name: 'Tibetan',
+    icon: '🇨🇳',
+  },
+  {
+    code: 'br',
+    name: 'Breton',
+    icon: '🇫🇷',
+  },
+  {
+    code: 'bs',
+    name: 'Bosnian',
+    icon: '🇧🇦',
+  },
+  {
+    code: 'ca',
+    name: 'Catalan; Valencian',
+    icon: '🇪🇸',
+  },
+  {
+    code: 'ce',
+    name: 'Chechen',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'ch',
+    name: 'Chamorro',
+    icon: '🇬🇺',
+  },
+  {
+    code: 'co',
+    name: 'Corsican',
+    icon: '🇫🇷',
+  },
+  {
+    code: 'cr',
+    name: 'Cree',
+    icon: '🇨🇦',
+  },
+  {
+    code: 'cs',
+    name: 'Czech',
+    icon: '🇨🇿',
+  },
+  {
+    code: 'cu',
+    name: 'Church Slavic; Old Slavonic; Church Slavonic; Old Bulgarian; Old Church Slavonic',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'cv',
+    name: 'Chuvash',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'cy',
+    name: 'Welsh',
+    icon: '🇬🇧',
+  },
+  {
+    code: 'da',
+    name: 'Danish',
+    icon: '🇩🇰',
+  },
+  {
+    code: 'de',
+    name: 'German',
+    icon: '🇩🇪',
+  },
+  {
+    code: 'dv',
+    name: 'Divehi; Dhivehi; Maldivian',
+    icon: '🇲🇻',
+  },
+  {
+    code: 'dz',
+    name: 'Dzongkha',
+    icon: '🇧🇹',
+  },
+  {
+    code: 'ee',
+    name: 'Ewe',
+    icon: '🇬🇭',
+  },
+  {
+    code: 'el',
+    name: 'Greek, Modern (1453-)',
+    icon: '🇬🇷',
+  },
+  {
+    code: 'en',
+    name: 'English',
+    icon: '🇺🇸',
+  },
+  {
+    code: 'eo',
+    name: 'Esperanto',
+    icon: '',
+  },
+  {
+    code: 'es',
+    name: 'Spanish; Castilian',
+    icon: '🇪🇸',
+  },
+  {
+    code: 'et',
+    name: 'Estonian',
+    icon: '🇪🇪',
+  },
+  {
+    code: 'eu',
+    name: 'Basque',
+    icon: '🇪🇸',
+  },
+  {
+    code: 'fa',
+    name: 'Persian',
+    icon: '🇮🇷',
+  },
+  {
+    code: 'ff',
+    name: 'Fulah',
+    icon: '🇸🇳',
+  },
+  {
+    code: 'fi',
+    name: 'Finnish',
+    icon: '🇫🇮',
+  },
+  {
+    code: 'fj',
+    name: 'Fijian',
+    icon: '🇫🇯',
+  },
+  {
+    code: 'fo',
+    name: 'Faroese',
+    icon: '🇫🇴',
+  },
+  {
+    code: 'fr',
+    name: 'French',
+    icon: '🇫🇷',
+  },
+  {
+    code: 'fy',
+    name: 'Western Frisian',
+    icon: '🇳🇱',
+  },
+  {
+    code: 'ga',
+    name: 'Irish',
+    icon: '🇮🇪',
+  },
+  {
+    code: 'gd',
+    name: 'Gaelic; Scomttish Gaelic',
+    icon: '🇬🇧',
+  },
+  {
+    code: 'gl',
+    name: 'Galician',
+    icon: '🇪🇸',
+  },
+  {
+    code: 'gn',
+    name: 'Guarani',
+    icon: '🇵🇾',
+  },
+  {
+    code: 'gu',
+    name: 'Gujarati',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'gv',
+    name: 'Manx',
+    icon: '🇮🇲',
+  },
+  {
+    code: 'ha',
+    name: 'Hausa',
+    icon: '🇳🇬',
+  },
+  {
+    code: 'he',
+    name: 'Hebrew',
+    icon: '🇮🇱',
+  },
+  {
+    code: 'hi',
+    name: 'Hindi',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'ho',
+    name: 'Hiri Motu',
+    icon: '🇵🇬',
+  },
+  {
+    code: 'hr',
+    name: 'Croatian',
+    icon: '🇭🇷',
+  },
+  {
+    code: 'ht',
+    name: 'Haitian; Haitian Creole',
+    icon: '🇭🇹',
+  },
+  {
+    code: 'hu',
+    name: 'Hungarian',
+    icon: '🇭🇺',
+  },
+  {
+    code: 'hy',
+    name: 'Armenian',
+    icon: '🇦🇲',
+  },
+  {
+    code: 'hz',
+    name: 'Herero',
+    icon: '🇳🇦',
+  },
+  {
+    code: 'ia',
+    name: 'Interlingua (International Auxiliary Language Association)',
+    icon: '🇫🇷',
+  },
+  {
+    code: 'id',
+    name: 'Indonesian',
+    icon: '🇮🇩',
+  },
+  {
+    code: 'ie',
+    name: 'Interlingue; Occidental',
+    icon: '',
+  },
+  {
+    code: 'ig',
+    name: 'Igbo',
+    icon: '🇳🇬',
+  },
+  {
+    code: 'ii',
+    name: 'Sichuan Yi; Nuosu',
+    icon: '🇨🇳',
+  },
+  {
+    code: 'ik',
+    name: 'Inupiaq',
+    icon: '🇺🇸',
+  },
+  {
+    code: 'io',
+    name: 'Ido',
+    icon: '',
+  },
+  {
+    code: 'is',
+    name: 'Icelandic',
+    icon: '🇮🇸',
+  },
+  {
+    code: 'it',
+    name: 'Italian',
+    icon: '🇮🇹',
+  },
+  {
+    code: 'iu',
+    name: 'Inuktitut',
+    icon: '🇨🇦',
+  },
+  {
+    code: 'ja',
+    name: 'Japanese',
+    icon: '🇯🇵',
+  },
+  {
+    code: 'jv',
+    name: 'Javanese',
+    icon: '🇮🇩',
+  },
+  {
+    code: 'ka',
+    name: 'Georgian',
+    icon: '🇬🇪',
+  },
+  {
+    code: 'kg',
+    name: 'Kongo',
+    icon: '🇨🇩',
+  },
+  {
+    code: 'ki',
+    name: 'Kikuyu; Gikuyu',
+    icon: '🇰🇪',
+  },
+  {
+    code: 'kj',
+    name: 'Kuanyama; Kwanyama',
+    icon: '🇳🇦',
+  },
+  {
+    code: 'kk',
+    name: 'Kazakh',
+    icon: '🇰🇿',
+  },
+  {
+    code: 'kl',
+    name: 'Kalaallisut; Greenlandic',
+    icon: '🇬🇱',
+  },
+  {
+    code: 'km',
+    name: 'Central Khmer',
+    icon: '🇰🇭',
+  },
+  {
+    code: 'kn',
+    name: 'Kannada',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'ko',
+    name: 'Korean',
+    icon: '🇰🇷',
+  },
+  {
+    code: 'kr',
+    name: 'Kanuri',
+    icon: '',
+  },
+  {
+    code: 'ks',
+    name: 'Kashmiri',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'ku',
+    name: 'Kurdish',
+    icon: '🇹🇷',
+  },
+  {
+    code: 'kv',
+    name: 'Komi',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'kw',
+    name: 'Cornish',
+    icon: '🇬🇧',
+  },
+  {
+    code: 'ky',
+    name: 'Kirghiz; Kyrgyz',
+    icon: '🇰🇬',
+  },
+  {
+    code: 'la',
+    name: 'Latin',
+    icon: '🇻🇦',
+  },
+  {
+    code: 'lb',
+    name: 'Luxembourgish; Letzeburgesch',
+    icon: '🇱🇺',
+  },
+  {
+    code: 'lg',
+    name: 'Ganda',
+    icon: '🇺🇬',
+  },
+  {
+    code: 'li',
+    name: 'Limburgan; Limburger; Limburgish',
+    icon: '🇳🇱',
+  },
+  {
+    code: 'ln',
+    name: 'Lingala',
+    icon: '🇨🇩',
+  },
+  {
+    code: 'lo',
+    name: 'Lao',
+    icon: '🇱🇦',
+  },
+  {
+    code: 'lt',
+    name: 'Lithuanian',
+    icon: '🇱🇹',
+  },
+  {
+    code: 'lu',
+    name: 'Luba-Katanga',
+    icon: '🇨🇩',
+  },
+  {
+    code: 'lv',
+    name: 'Latvian',
+    icon: '🇱🇻',
+  },
+  {
+    code: 'mg',
+    name: 'Malagasy',
+    icon: '🇲🇬',
+  },
+  {
+    code: 'mh',
+    name: 'Marshallese',
+    icon: '🇲🇭',
+  },
+  {
+    code: 'mi',
+    name: 'Maori',
+    icon: '🇳🇿',
+  },
+  {
+    code: 'mk',
+    name: 'Macedonian',
+    icon: '🇲🇰',
+  },
+  {
+    code: 'ml',
+    name: 'Malayalam',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'mn',
+    name: 'Mongolian',
+    icon: '🇲🇳',
+  },
+  {
+    code: 'mr',
+    name: 'Marathi',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'ms',
+    name: 'Malay',
+    icon: '🇲🇾',
+  },
+  {
+    code: 'mt',
+    name: 'Maltese',
+    icon: '🇲🇹',
+  },
+  {
+    code: 'my',
+    name: 'Burmese',
+    icon: '🇲🇲',
+  },
+  {
+    code: 'na',
+    name: 'Nauru',
+    icon: '🇳🇷',
+  },
+  {
+    code: 'nb',
+    name: 'Bokmål, Norwegian; Norwegian Bokmål',
+    icon: '🇳🇴',
+  },
+  {
+    code: 'nd',
+    name: 'Ndebele, North; North Ndebele',
+    icon: '🇿🇼',
+  },
+  {
+    code: 'ne',
+    name: 'Nepali',
+    icon: '🇳🇵',
+  },
+  {
+    code: 'ng',
+    name: 'Ndonga',
+    icon: '🇳🇦',
+  },
+  {
+    code: 'nl',
+    name: 'Dutch; Flemish',
+    icon: '🇳🇱',
+  },
+  {
+    code: 'nn',
+    name: 'Norwegian Nynorsk; Nynorsk, Norwegian',
+    icon: '🇳🇴',
+  },
+  {
+    code: 'no',
+    name: 'Norwegian',
+    icon: '🇳🇴',
+  },
+  {
+    code: 'nr',
+    name: 'Ndebele, South; South Ndebele',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'nv',
+    name: 'Navajo; Navaho',
+    icon: '🇺🇸',
+  },
+  {
+    code: 'ny',
+    name: 'Chichewa; Chewa; Nyanja',
+    icon: '🇲🇼',
+  },
+  {
+    code: 'oc',
+    name: 'Occitan (post 1500)',
+    icon: '🇫🇷',
+  },
+  {
+    code: 'oj',
+    name: 'Ojibwa',
+    icon: '',
+  },
+  {
+    code: 'om',
+    name: 'Oromo',
+    icon: '🇪🇹',
+  },
+  {
+    code: 'or',
+    name: 'Oriya',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'os',
+    name: 'Ossetian; Ossetic',
+    icon: '🇬🇪',
+  },
+  {
+    code: 'pa',
+    name: 'Panjabi; Punjabi',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'pi',
+    name: 'Pali',
+    icon: '',
+  },
+  {
+    code: 'pl',
+    name: 'Polish',
+    icon: '🇵🇱',
+  },
+  {
+    code: 'ps',
+    name: 'Pushto; Pashto',
+    icon: '🇦🇫',
+  },
+  {
+    code: 'pt',
+    name: 'Portuguese',
+    icon: '🇧🇷',
+  },
+  {
+    code: 'qu',
+    name: 'Quechua',
+    icon: '🇵🇪',
+  },
+  {
+    code: 'rm',
+    name: 'Romansh',
+    icon: '🇨🇭',
+  },
+  {
+    code: 'rn',
+    name: 'Rundi',
+    icon: '🇧🇮',
+  },
+  {
+    code: 'ro',
+    name: 'Romanian; Moldavian; Moldovan',
+    icon: '🇷🇴',
+  },
+  {
+    code: 'ru',
+    name: 'Russian',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'rw',
+    name: 'Kinyarwanda',
+    icon: '🇷🇼',
+  },
+  {
+    code: 'sa',
+    name: 'Sanskrit',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'sc',
+    name: 'Sardinian',
+    icon: '🇮🇹',
+  },
+  {
+    code: 'sd',
+    name: 'Sindhi',
+    icon: '🇵🇰',
+  },
+  {
+    code: 'se',
+    name: 'Northern Sami',
+    icon: '🇳🇴',
+  },
+  {
+    code: 'sg',
+    name: 'Sango',
+    icon: '🇨🇫',
+  },
+  {
+    code: 'si',
+    name: 'Sinhala; Sinhalese',
+    icon: '🇱🇰',
+  },
+  {
+    code: 'sk',
+    name: 'Slovak',
+    icon: '🇸🇰',
+  },
+  {
+    code: 'sl',
+    name: 'Slovenian',
+    icon: '🇸🇮',
+  },
+  {
+    code: 'sm',
+    name: 'Samoan',
+    icon: '🇼🇸',
+  },
+  {
+    code: 'sn',
+    name: 'Shona',
+    icon: '🇿🇼',
+  },
+  {
+    code: 'so',
+    name: 'Somali',
+    icon: '🇸🇴',
+  },
+  {
+    code: 'sq',
+    name: 'Albanian',
+    icon: '🇦🇱',
+  },
+  {
+    code: 'sr',
+    name: 'Serbian',
+    icon: '🇷🇸',
+  },
+  {
+    code: 'ss',
+    name: 'Swati',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'st',
+    name: 'Sotho, Southern',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'su',
+    name: 'Sundanese',
+    icon: '🇮🇩',
+  },
+  {
+    code: 'sv',
+    name: 'Swedish',
+    icon: '🇸🇪',
+  },
+  {
+    code: 'sw',
+    name: 'Swahili',
+    icon: '🇹🇿',
+  },
+  {
+    code: 'ta',
+    name: 'Tamil',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'te',
+    name: 'Telugu',
+    icon: '🇮🇳',
+  },
+  {
+    code: 'tg',
+    name: 'Tajik',
+    icon: '🇹🇯',
+  },
+  {
+    code: 'th',
+    name: 'Thai',
+    icon: '🇹🇭',
+  },
+  {
+    code: 'ti',
+    name: 'Tigrinya',
+    icon: '🇪🇹',
+  },
+  {
+    code: 'tk',
+    name: 'Turkmen',
+    icon: '🇹🇲',
+  },
+  {
+    code: 'tl',
+    name: 'Tagalog',
+    icon: '🇵🇭',
+  },
+  {
+    code: 'tn',
+    name: 'Tswana',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'to',
+    name: 'Tonga (Tonga Islands)',
+    icon: '🇹🇴',
+  },
+  {
+    code: 'tr',
+    name: 'Turkish',
+    icon: '🇹🇷',
+  },
+  {
+    code: 'ts',
+    name: 'Tsonga',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'tt',
+    name: 'Tatar',
+    icon: '🇷🇺',
+  },
+  {
+    code: 'tw',
+    name: 'Twi',
+    icon: '',
+  },
+  {
+    code: 'ty',
+    name: 'Tahitian',
+    icon: '🇵🇫',
+  },
+  {
+    code: 'ug',
+    name: 'Uighur; Uyghur',
+    icon: '🇨🇳',
+  },
+  {
+    code: 'uk',
+    name: 'Ukrainian',
+    icon: '🇺🇦',
+  },
+  {
+    code: 'ur',
+    name: 'Urdu',
+    icon: '🇵🇰',
+  },
+  {
+    code: 'uz',
+    name: 'Uzbek',
+    icon: '🇺🇿',
+  },
+  {
+    code: 've',
+    name: 'Venda',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'vi',
+    name: 'Vietnamese',
+    icon: '🇻🇳',
+  },
+  {
+    code: 'vo',
+    name: 'Volapük',
+    icon: '',
+  },
+  {
+    code: 'wa',
+    name: 'Walloon',
+    icon: '🇧🇪',
+  },
+  {
+    code: 'wo',
+    name: 'Wolof',
+    icon: '🇸🇳',
+  },
+  {
+    code: 'xh',
+    name: 'Xhosa',
+    icon: '🇿🇦',
+  },
+  {
+    code: 'yi',
+    name: 'Yiddish',
+    icon: '',
+  },
+  {
+    code: 'yo',
+    name: 'Yoruba',
+    icon: '🇳🇬',
+  },
+  {
+    code: 'za',
+    name: 'Zhuang; Chuang',
+    icon: '🇨🇳',
+  },
+  {
+    code: 'zh',
+    name: 'Chinese',
+    icon: '🇨🇳',
+  },
+  {
+    code: 'zu',
+    name: 'Zulu',
+    icon: '🇿🇦',
+  },
+];
